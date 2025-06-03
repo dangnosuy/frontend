5472738ba53ce4c97ebac60e139e247897b8b571
dangnosuy <125873316+dangnosuy@users.noreply.github.com>
2025-06-03 15:32:23 +0700

    Update header.js

--- js/header.js
+++ js/header.js
@@ -75,7 +75,35 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const balance = document.getElementById('user-balance');
 
+    const username = localStorage.getItem('username')
+    console.log(username)
+
+    fetch(`https://68cfba9e-fe18-4301-8a54-7a4bfc790350-00-26ktjx6yhw4zw.pike.replit.dev/api/get_balance?username=${username}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => response.json())
+        .then(jsonData => {
+            console.log(jsonData);
+            if (jsonData.success === true) {
+                const rawBalance = jsonData.balance.balance;
+
+                // Nếu muốn convert thành Number để format:
+                const num = parseFloat(rawBalance);
+
+                balance.textContent = num.toLocaleString() + ' VND'
+            }
+            else {
+                console.log("Error: ", jsonData.error)
+            }
+        })
+        .catch(err => console.error("Fetch error: ", err));
+})
 // Auto logout when idle
 const MAX_IDLE_TIME = 10000000 * 1000; 
 let idleTimer;
